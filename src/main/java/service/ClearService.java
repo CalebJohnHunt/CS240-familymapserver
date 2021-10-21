367523d6aee68ee61935cbfe027ebdf06de896fc
Caleb <kaychubyt@gmail.com>
2021-10-21 02:19:26 -0600

    Implemented Clear and Register services

--- src/main/java/service/ClearService.java
+++ src/main/java/service/ClearService.java
@@ -1,15 +1,28 @@
 package service;
 
+import dao.DataAccessException;
+import dao.Database;
 import service.result.ClearResult;
 
 /**
  * Service for /clear.
  */
 public class ClearService {
+    private final Database db = new Database();
 
     /**
      * Calls the /clear API.
      * @return the response from the API.
      */
-    public ClearResult clear() { return null; }
+    public ClearResult clear() throws DataAccessException {
+        try {
+            db.openConnection();
+            db.clearTables();
+            db.closeConnection(true);
+            return new ClearResult(true, "Clear succeeded");
+        } catch (DataAccessException e) {
+            db.closeConnection(false);
+            return new ClearResult(false, "Error: Failed to clear table");
+        }
+    }
 }
--- src/main/java/service/RegisterService.java
+++ src/main/java/service/RegisterService.java
@@ -1,17 +1,61 @@
 package service;
 
+import dao.*;
+import model.AuthToken;
+import model.Person;
+import model.User;
 import service.request.RegisterRequest;
 import service.result.RegisterResult;
+import utility.IDGenerator;
+
+// TODO: Fill the user with fake data (i.e. give them parents and events and stuff) - Make helper class
 
 /**
  * Service for /user/register.
  */
 public class RegisterService {
+    private final Database db = new Database();
 
     /**
-     * Calls the /user/register API.
+     * Registers a new user. Inserts a new user into the database, generates their personID and an AuthToken.
      * @param request the request to the API.
      * @return the response from the API.
      */
-    public RegisterResult register(RegisterRequest request) { return null; }
+    public RegisterResult register(RegisterRequest request) throws DataAccessException {
+        UserDAO uDao = new UserDAO(db.getConnection()); // starts transaction
+        User newUser = new User(request.getUsername(), request.getPassword(), request.getEmail(), request.getFirstName(),
+                request.getLastName(), request.getGender(), IDGenerator.generateID());
+        try {
+            uDao.insert(newUser);
+            // Make authToken for the user
+            String authTokenID = insertAuthTokenForNewUser(newUser.getUsername());
+            insertPersonForNewUser(newUser);
+            db.closeConnection(true);
+            return new RegisterResult(authTokenID, newUser.getUsername(), newUser.getPersonID());
+
+        } catch (DataAccessException e) {
+            db.closeConnection(false);
+            return new RegisterResult("Couldn't register user.");
+        }
+
+    }
+
+    /**
+     * Create and insert an AuthToken for the user getting registered.
+     * @param username the username of the user getting registered
+     * @return the new authToken's ID
+     */
+    private String insertAuthTokenForNewUser(String username) throws DataAccessException {
+        AuthTokenDAO atDAO = new AuthTokenDAO(db.getConnection());
+        String authTokenID = IDGenerator.generateID();
+        atDAO.insert(new AuthToken(authTokenID, username));
+        return authTokenID;
+    }
+
+    private void insertPersonForNewUser(User newUser) throws DataAccessException {
+        PersonDAO pDAO = new PersonDAO(db.getConnection());
+        // TODO: Probably don't want null for father, mother, spouse. But that's the same thing as above: generate data
+        pDAO.insert(new Person(newUser.getPersonID(), newUser.getUsername(), newUser.getFirstName(), newUser.getLastName(),
+                newUser.getGender(), null, null, null));
+    }
 }
